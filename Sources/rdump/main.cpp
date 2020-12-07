d45d412cc3c4f9a1ca6e059eef9420d748d822d2
caoming.roy <caoming.roy@bytedance.com>
2020-12-07 21:29:53 +0800

    Remove MachODump

--- Sources/rdump/main.cpp
+++ Sources/rdump/main.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
 #include "llvm/Support/CommandLine.h"
 #include "llvm/ADT/STLExtras.h"
-#include "MachODump.h"
+#include "MachOObjectFile.h"
 
 using namespace llvm;
+using namespace macho;
 
 static cl::opt<std::string> Library("library",
                                     cl::Optional,
@@ -14,6 +16,13 @@ static cl::list<std::string> InputFilenames(cl::Optional,
                                             cl::ZeroOrMore,
                                             cl::Optional);
 
+static cl::list<std::string> Arch("arch",
+                                  cl::Optional,
+                                  cl::desc("Target arch to disassemble for, "
+                                           "see -version for available targets"),
+                                  cl::ZeroOrMore,
+                                  cl::Optional);
+
 /// Open file and figure out how to dump it.
 static void dumpInput(StringRef file) {
   
@@ -22,7 +31,9 @@ static void dumpInput(StringRef file) {
 int main(int argc, char *argv[]) {
   cl::ParseCommandLineOptions(argc, argv);
   
-  llvm::for_each(InputFilenames, dumpInput);
+  Optional<MemoryBufferRef> buffer = readFile(Library);
+  
+  // llvm::for_each(InputFilenames, dumpInput);
   
   return EXIT_SUCCESS;
 }
