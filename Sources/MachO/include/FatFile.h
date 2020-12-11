c55ea93831a026442baa7003483c1571a8ecebdd
zhuxuanlin <zhuxuanlin@bytedance.com>
2020-12-11 17:47:28 +0800

    Fix ReadFile Method.

--- Sources/MachO/include/FatFile.h
+++ Sources/MachO/include/FatFile.h
@@ -1,13 +1,31 @@
-#ifndef FatFile_hpp
-#define FatFile_hpp
+#ifndef FatFile_h
+#define FatFile_h
 
 #include <stdio.h>
+#include "llvm/BinaryFormat/MachO.h"
+#include "llvm/Support/MemoryBuffer.h"
 #include "File.h"
+
+using namespace llvm;
 
 namespace macho {
 
 class FatFile : public File {
+  uint32_t Magic;
+  uint32_t NumberOfObjects;
   
+public:
+  static constexpr uint32_t MaxSectionAlignment = 15;
+  
+  class ObjectForArch {
+    // ...
+  };
+  
+  class object_iterator {
+    // ...
+  };
+  
+  FatFile(MemoryBufferRef Souce, Error &Err);
 };
 
 };
